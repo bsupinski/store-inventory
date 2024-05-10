235b90c64252f22f3a6985f843572addb266e05d
Brandon Supinski <66806979+bsupinski@users.noreply.github.com>
2024-05-10 15:49:37 -0400

    Added viewing product function

--- store/model.py
+++ store/model.py
@@ -13,15 +13,15 @@ Base = declarative_base()
 class Product(Base):
     __tablename__ = "inventory"
     
-    id = Column(Integer, primary_key=True)
-    name = Column("Product Name", String)
-    price = Column("Price", Integer)
-    quanity = Column("Quanity", Integer)
-    date = Column("Last Updated", Date)
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column("Product Name", String)
+    product_price = Column("Price", Integer)
+    product_quantity = Column("Quanity", Integer)
+    date_updated  = Column("Last Updated", Date)
     
     
     def __repr__(self):
-        return f"Name: {self.name} Price: {self.price} Quanity: {self.quanity} Last Updated: {self.date}"
+        return f"Name: {self.product_name} Price: {self.product_price} Quanity: {self.product_quantity} Last Updated: {self.date_updated}"
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
--- store/store_keeper.py
+++ store/store_keeper.py
@@ -8,7 +8,7 @@ class Store_Keeper():
     def clean_price(self, price_str):
         float_price = price_str.replace("$", "")
         float_price = float(float_price)
-        return float_price
+        return int(float_price * 100)
     
     
     def clean_date(self, date_str):
@@ -19,20 +19,54 @@ class Store_Keeper():
         return_Date = datetime.date(year, month, day)
         
         return return_Date
-        
+    
+    
+    def view_product(self):
+        available_products = []
+        for product in session.query(Product):
+            available_products.append(product.product_id)
+        error_warning = True
+        while error_warning:
+            try:
+                
+                print(f'''\nAvailable product IDs\r{available_products}''')
+                user_choice = input("Select what product ID you would like to view.  ")
+                user_choice = int(user_choice)
+                view_product = session.query(Product).filter(Product.product_id==int(user_choice)).first()
+                print(f'''
+                    \nName: {view_product.product_name}
+                    \rQuanity: {view_product.product_quantity}
+                    \rPrice: ${view_product.product_price / 100}
+                    \rLast Updated: {datetime.date.strftime(view_product.date_updated, "%b %d, %Y")}
+                    ''')
+                error_warning = False
+                
+            except ValueError:
+                input(f'''You entered an incorrect value. Press "Enter" to try again. ''')
+            except AttributeError:
+                 input(f'''You entered an incorrect value. Press "Enter" to try again. ''')
+        print("Outside the While block")
+
+
+def edit_product(self):
+    available_inputs =["1", "2", "3"]
+    user_input = input('''\rWhat would you like to do?
+                       \n   1. Edit Product
+                       \n   2. Check Profuct Profit''')
+
 
     def add_csv(self):
         with open("../inventory.csv") as csvfile:
             data = csv.reader(csvfile)
             next(data)
             for row in data:
-                product_in_db = session.query(Product).filter(Product.name==row[0]).one_or_none()
+                product_in_db = session.query(Product).filter(Product.product_name==row[0]).one_or_none()
                 if product_in_db == None:
                     name = row[0]
                     price = self.clean_price(row[1])
                     quanity = row[2]
                     date = self.clean_date(row[3])
-                    new_product = Product(name=name, price=price, quanity=quanity, date=date)
+                    new_product = Product(product_name=name, product_price=price, product_quantity=quanity, date_updated=date)
                     session.add(new_product)
             session.commit()
             
@@ -43,3 +77,4 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     newstore = Store_Keeper()
     newstore.add_csv()
+    newstore.view_product()
